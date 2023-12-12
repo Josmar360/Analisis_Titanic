7bebc42fb8634e60b0dcf1d9c51c0e09b81192d5
Josmar360 <josmargustavopalominocastelan@gmail.com>
2023-12-12 14:23:57 -0600

    Comparacion de data sets

--- 4) Limpieza de datos/5) Examinar_Valores.py
+++ 4) Limpieza de datos/5) Examinar_Valores.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
 # Cargar los archivos CSV
-df_old = pd.read_csv("archivo_antiguo.csv")
+df_old = pd.read_csv("4) Limpieza de datos/Titanic-Dataset-old.csv")
 df_new = pd.read_csv("4) Limpieza de datos/Titanic-Dataset.csv")
+
+# Compara los valores agregados
+print(df_new.describe())
+print(df_old.describe())
