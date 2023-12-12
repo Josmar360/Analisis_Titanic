5df3b2b90ac0556f8f4e477a1429fe9188097a9c
Josmar360 <josmargustavopalominocastelan@gmail.com>
2023-12-12 13:51:25 -0600

    Correccion de aplicacion de valores numericos

--- 4) Limpieza de datos/3) One-hot.py
+++ 4) Limpieza de datos/3) One-hot.py
@@ -6,8 +6,8 @@ data = pd.read_csv("4) Limpieza de datos/Titanic-Dataset.csv")
 # Aplicar One-Hot a la variable Sex
 data = pd.get_dummies(data, columns=['Sex'])
 
-# Asignación de números a la variable 'Pclass'
+# Asignación de números a la variable 'Pclass' (Label Encoding)
 data['Pclass'] = data['Pclass'].astype('category').cat.codes
 
-# Sobrescribir el archivo SCV
-data.to_csv("4) Limpieza de datos/Titanic-Dataset.csv", index=False)
+# Asignación de números a la variable 'Embarked' (Label Encoding)
+data['Embarked'] = data['Embarked'].astype('category').cat.codes
