a66f3bc38a46ef7220819f2ae733be9a8056e897
Josmar360 <josmargustavopalominocastelan@gmail.com>
2023-12-16 02:14:20 -0600

    Histrograma de edades

---
+++ 5) Visualizacion de datos/1) Histograma_Edades.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Abrir el archivo CSV
+data = pd.read_csv("4) Limpieza de datos/Titanic-Dataset.csv", index_col='PassengerId')
+
+# Configuración del estilo de seaborn para gráficos más atractivos
+sns.set(style="whitegrid")
+
+# Crear el histograma utilizando seaborn
+plt.figure(figsize=(10, 6))
+sns.histplot(data['Age'], bins=30, kde=True, color='skyblue')
+plt.title('Histograma de Edades')
+plt.xlabel('Edad')
+plt.ylabel('Frecuencia')
+plt.show()
