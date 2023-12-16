ee73eb68d7ddfbc0a0eac41fc03c7ce7d94fab63
Josmar360 <josmargustavopalominocastelan@gmail.com>
2023-12-16 02:30:26 -0600

    Histograma de genero y supervivencia2

---
+++ 5) Visualizacion de datos/4) Genero_Supervivencia.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Abrir el archivo CSV
+data = pd.read_csv("4) Limpieza de datos/Titanic-Dataset.csv", index_col='PassengerId')
+
+# Crear un gráfico de barras
+sns.set(style="whitegrid")
+plt.figure(figsize=(8, 6))
+
+# Usar seaborn para crear un gráfico de barras agrupado por género y supervivencia
+sns.countplot(x="Survived", hue="Sex_female", data=data)
+
+# Personalizar el gráfico
+plt.title('Comparación entre Género y Supervivencia')
+plt.xlabel('Supervivencia (0 = No, 1 = Sí)')
+plt.ylabel('Cantidad de Pasajeros')
+plt.legend(title='Género', labels=['Masculino', 'Femenino'])
+
+# Mostrar el gráfico
+plt.show()
